74fdb4f59ab15bff478d0e1cce6c6bf1e4c6efbf
Ajayantony-M <ajayantony2003m@gmail.com>
2025-06-07 12:59:03 +0530

    Menu now showing Tamil and English

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1420678142"
+    serviceWorkerVersion: "3686927052"
   }
 });
